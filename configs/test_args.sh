3392856843c6ab85ab373d0c9b2cc4e34aa8ae1f
toicca <nico.toikka@helsinki.fi>
2024-04-09 14:43:01 +0200

    Fixed some bin labels

--- configs/test_args.sh
+++ configs/test_args.sh
@@ -2,4 +2,4 @@
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_105a/x86_64-el9-gcc13-opt/setup.sh
 
-python3 src/main.py --filepath data/testfiles.txt --triggerpath data/testtrigger.txt --output_path out --run_id test_args_mj --nThreads 8 --progress_bar --is_local
+python3 src/main.py --filepath data/testfiles.txt --triggerpath data/testtrigger.txt --output_path out --run_id rerun --nThreads 8 --number_of_files 5 --progress_bar --is_local
