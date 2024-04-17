ff156ed464a05c5765ca6d1bb315738469ee52a5
Dan Näsman <Mdan.nasman@gmail.com>
2024-04-17 11:26:49 +0200

    change plotting code

--- src/plotting/produce_plots.py
+++ src/plotting/produce_plots.py
@@ -20,13 +20,11 @@ def parse_arguments():
     files.add_argument("--filelist", type=str, help="Comma separated list of root files produced by dijet_rdf")
     files.add_argument("--filepath", type=str, help="Path to a root file containing a list of output files produced by dijet_rdf")
 
-    triggers = parser.add_mutually_exclusive_group()
-    triggers.add_argument("--triggerlist", type=str, help="Comma separated list of triggers for which plots will be produced (default value 'all')")
-    triggers.add_argument("--triggerpath", type=str, help="Path to a file containing a list of triggers for which plots will be produced")
-
-    parser.add_argument("--out", type=str, default="", help="Output path")
+    parser.add_argument("--out", required=True, type=str, help="Output path")
 
     parser.add_argument("--config", type=str, default="", help="Path to config file")
+
+    parser.add_argument("--all", action="store_true", help="Produce all plots in given .root files")
 
     args = parser.parse_args()
     
@@ -38,21 +36,19 @@ def read_config_file(file: str) -> configparser.ConfigParser:
 
     return config
 
-
-def produce_plots(file, output_path, config, trigger_list=[]):
-    triggers = trigger_list
-    if len(triggers) == 0:
-        trigger_keys = file.GetListOfKeys()
-        triggers = [tkey.GetName() for tkey in trigger_keys]
-
-    for trigger in triggers:
-        systems = file.Get(trigger)
+def produce_plots_all(file, output_path):
+    subfolder_name = file.GetName().split("/")[-1].replace(".root", "").replace(".", "_")
+    for trigger_key in file.GetListOfKeys():
+        trigger_name = trigger_key.GetName()
+        systems = file.Get(trigger_name)
         for system_key in systems.GetListOfKeys():
             system_name = system_key.GetName()
             methods = systems.Get(system_name)
             for method_key in methods.GetListOfKeys():
                 method_name = method_key.GetName()
-                pathlib.Path("{}/{}/{}/{}".format(output_path, trigger, system_name, method_name)).mkdir(exist_ok=True, parents=True)
+
+                pathlib.Path("{}/{}/{}/{}/{}".format(output_path, subfolder_name, trigger_name, system_name, method_name)).mkdir(exist_ok=True, parents=True)
+
                 hists = methods.Get(method_name)
                 for hist_key in hists.GetListOfKeys():
                     hist_name = hist_key.GetName()
@@ -62,63 +58,25 @@ def produce_plots(file, output_path, config, trigger_list=[]):
                     if hist.InheritsFrom("TH3D") or hist.InheritsFrom("TProfile3D"):
                         continue
 
-                    xlabel = hist.GetXaxis().GetTitle()
-                    ylabel = hist.GetYaxis().GetTitle()
+                    xtitle = hist.GetXaxis().GetTitle()
+                    ytitle = hist.GetYaxis().GetTitle()
 
+                    (b_min, b_max) = (hist.GetMinimumBin(), hist.GetMaximumBin())
                     (x_min, x_max) = (hist.GetXaxis().GetXmin(), hist.GetXaxis().GetXmax())
-                    (y_min, y_max) = (hist.GetYaxis().GetXmin(), hist.GetYaxis().GetXmax())
-
-                    if hist.InheritsFrom("TH1D") or hist.InheritsFrom("TProfile1D"):
-                        (b_min, b_max) = (hist.GetMinimumBin(), hist.GetMaximumBin())
-                        (x_min, x_max) = (hist.GetXaxis().GetXmin(), hist.GetXaxis().GetXmax())
-                        (y_min, y_max) = (hist.GetBinContent(b_min), hist.GetBinContent(b_max))
-                        y_max = 1.05*y_max
-
+                    (y_min, y_max) = (hist.GetBinContent(b_min), hist.GetBinContent(b_max))
+                    y_max = 1.05*y_max
                     iPos = 33
 
                     # Move CMS logo/text out of frame so it does not get covered by the plot
                     if hist.InheritsFrom("TH2D") or hist.InheritsFrom("TProfile2D"):
+                        (x_min, x_max) = (hist.GetXaxis().GetXmin(), hist.GetXaxis().GetXmax())
+                        (y_min, y_max) = (hist.GetYaxis().GetXmin(), hist.GetYaxis().GetXmax())
                         iPos = 0
 
                     CMS.SetExtraText("Private")
                     CMS.SetEnergy("13.6")
                     CMS.SetLumi("")
-                    canv = CMS.cmsCanvas('', x_min, x_max, y_min, y_max, xlabel, ylabel, square = True, extraSpace=0.06, iPos=iPos)
-
-                    if config.has_section(hist_name):
-                        logx = int(config[hist_name]["logx"])
-                        logy = int(config[hist_name]["logy"])
-
-                        if config[hist_name]["xlim"] != "":
-                            (x_min, x_max) = tuple(map(float, config[hist_name]["xlim"].split(" ")))
-
-                        if config[hist_name]["ylim"] != "":
-                            (y_min, y_max) = tuple(map(float, config[hist_name]["ylim"].split(" ")))
-
-                        if config[hist_name]["xlabel"] != "":
-                            xlabel = config[hist_name]["xlabel"]
-
-                        if config[hist_name]["ylabel"] != "":
-                            xlabel = config[hist_name]["ylabel"]
-
-                        if config[hist_name]["iPos"] != "":
-                            iPos = int(config[hist_name]["iPos"])
-
-                        canv = CMS.cmsCanvas('', x_min, x_max, y_min, y_max, xlabel, ylabel, square = True, extraSpace=0.06, iPos=0)
-
-                        if logx == 1:
-                            canv.SetLogx()
-
-                        if logy == 1:
-                            canv.SetLogy()
-
-                        if config[hist_name]["xlabelsize"] != "":
-                            xlabelsize = float(config[hist_name]["xlabelsize"])
-                            CMS.GetcmsCanvasHist(canv).GetXaxis().SetLabelSize(xlabelsize)
-
-                        if config[hist_name]["ylabelsize"] != "":
-                            ylabelsize = float(config[hist_name]["ylabelsize"])
-                            CMS.GetcmsCanvasHist(canv).GetYaxis().SetLabelSize(ylabelsize)
+                    canv = CMS.cmsCanvas('', x_min, x_max, y_min, y_max, xtitle, ytitle, square = True, extraSpace=0.0, iPos=iPos)
 
                     CMS.GetcmsCanvasHist(canv).GetYaxis().SetTitleOffset(1.5)
                     CMS.GetcmsCanvasHist(canv).GetXaxis().SetTitleOffset(0.9)
@@ -134,18 +92,131 @@ def produce_plots(file, output_path, config, trigger_list=[]):
                     CMS.cmsDraw(hist, "", marker, msize, mcolor, lstyle, lwidth, lcolor, fstyle, fcolor)
                     canv.Draw()
 
-                    CMS.SaveCanvas(canv, "{}/{}/{}/{}/{}.pdf".format(output_path, trigger, system_name, method_name, hist_name))
+                    CMS.SaveCanvas(canv, "{}/{}/{}/{}/{}/{}.pdf".format(output_path, subfolder_name, trigger_name, system_name, method_name, hist_name))
+
+def produce_plots_from_config(file, output_path, config, plots):
+    subfolder_name = file.GetName().split("/")[-1].replace(".root", "").replace(".", "_")
+    for plot_name in plots:
+        if config[plot_name]["triggers"] == "":
+            trigger_keys = file.GetListOfKeys()
+            triggers = [tkey.GetName() for tkey in trigger_keys]
+        else:
+            triggers = [s.strip() for s in config[plot_name]["triggers"].split(",")]
+
+        hists_config = [s.strip() for s in config[plot_name]["hists"].split(",")]
+
+        for trigger in triggers:
+            systems = file.Get(trigger)
+            collected_plots = []
+            for system_key in systems.GetListOfKeys():
+                system_name = system_key.GetName()
+                methods = systems.Get(system_name)
+                for method_key in methods.GetListOfKeys():
+                    method_name = method_key.GetName()
+                    hists = methods.Get(method_name)
+                    for hist_key in hists.GetListOfKeys():
+                        hist_name = hist_key.GetName()
+                        method_hist_str = f"{method_name}/{hist_name}"
+                        system_method_hist_str = f"{system_name}/{method_name}/{hist_name}"
+                        if hist_name in hists_config or method_hist_str in hists_config or system_method_hist_str in hists_config:
+                            hist = hists.Get(hist_name)
+                            collected_plots.append(hist)
+            
+            if len(collected_plots) == 0: continue
+            
+            pathlib.Path("{}/{}/{}".format(output_path, subfolder_name, trigger)).mkdir(exist_ok=True, parents=True)
+
+            xtitle = collected_plots[0].GetXaxis().GetTitle()
+            ytitle = collected_plots[0].GetYaxis().GetTitle()
+
+            (x_min, x_max) = (min([p.GetXaxis().GetXmin() for p in collected_plots]), max([p.GetXaxis().GetXmax() for p in collected_plots]))
+            (y_min, y_max) = (min([p.GetYaxis().GetXmin() for p in collected_plots]), max([p.GetYaxis().GetXmax() for p in collected_plots]))
+
+            if hist.InheritsFrom("TH1D") or hist.InheritsFrom("TProfile1D"):
+                (x_min, x_max) = (min([p.GetXaxis().GetXmin() for p in collected_plots]), max([p.GetXaxis().GetXmax() for p in collected_plots]))
+                (y_min, y_max) = (min([p.GetBinContent(p.GetMinimumBin()) for p in collected_plots]), max([p.GetBinContent(p.GetMaximumBin()) for p in collected_plots]))
+                y_max = 1.05*y_max
+
+            iPos = 33
+            if collected_plots[0].InheritsFrom("TH2D") or collected_plots[0].InheritsFrom("TProfile2D"):
+                iPos = 0
+
+            extraSpace = 0.0
+
+            CMS.SetExtraText("Private")
+            CMS.SetEnergy("13.6")
+            if config[plot_name]["energy"] != "":
+                CMS.SetEnergy(config[plot_name]["energy"])
+            
+            CMS.SetLumi("")
+            logx = int(config[plot_name]["logx"])
+            logy = int(config[plot_name]["logy"])
+
+            if config[plot_name]["xlim"] != "":
+                (x_min, x_max) = tuple(map(float, config[plot_name]["xlim"].split(" ")))
+
+            if config[plot_name]["ylim"] != "":
+                (y_min, y_max) = tuple(map(float, config[plot_name]["ylim"].split(" ")))
+
+            if config[plot_name]["xtitle"] != "":
+                xtitle = config[plot_name]["xtitle"]
+
+            if config[plot_name]["ytitle"] != "":
+                ytitle = config[plot_name]["ytitle"]
+
+            if config[plot_name]["iPos"] != "":
+                iPos = int(config[plot_name]["iPos"])
+
+            if config[plot_name]["extraSpace"] != "":
+                extraSpace = float(config[plot_name]["extraSpace"])
+
+            canv = CMS.cmsCanvas('', x_min, x_max, y_min, y_max, xtitle, ytitle, square=True, extraSpace=extraSpace, iPos=iPos)
+            if logx == 1:
+                canv.SetLogx()
+            if logy == 1:
+                canv.SetLogy()
+
+            if config[plot_name]["xlabelsize"] != "":
+                xlabelsize = float(config[plot_name]["xlabelsize"])
+                CMS.GetcmsCanvasHist(canv).GetXaxis().SetLabelSize(xlabelsize)
+
+            if config[plot_name]["ylabelsize"] != "":
+                ylabelsize = float(config[plot_name]["ylabelsize"])
+                CMS.GetcmsCanvasHist(canv).GetYaxis().SetLabelSize(ylabelsize)
+
+            if config[plot_name]["xtitlesize"] != "":
+                xtitlesize = float(config[plot_name]["xtitlesize"])
+                CMS.GetcmsCanvasHist(canv).GetXaxis().SetTitleSize(xtitlesize)
+
+            if config[plot_name]["ytitlesize"] != "":
+                ytitlesize = float(config[plot_name]["ytitlesize"])
+                CMS.GetcmsCanvasHist(canv).GetYaxis().SetTitleSize(ytitlesize)
+
+            if config[plot_name]["xtitleoffset"] != "":
+                xtitleoffset = float(config[plot_name]["xtitleoffset"])
+                CMS.GetcmsCanvasHist(canv).GetXaxis().SetTitleOffset(xtitleoffset)
+
+            if config[plot_name]["ytitleoffset"] != "":
+                xtitleoffset = float(config[plot_name]["ytitleoffset"])
+                CMS.GetcmsCanvasHist(canv).GetYaxis().SetTitleOffset(ytitleoffset)
+
+            marker = hist.GetMarkerStyle()
+            msize = hist.GetMarkerSize()
+            mcolor = hist.GetMarkerColor()
+            lstyle = hist.GetLineStyle()
+            lwidth = hist.GetLineWidth()
+            lcolor = hist.GetLineColor()
+            fstyle = hist.GetFillStyle()
+            fcolor = hist.GetFillColor()
+            for plot in collected_plots:
+                CMS.cmsDraw(plot, "", marker, msize, mcolor, lstyle, lwidth, lcolor, fstyle, fcolor)
+            canv.Draw()
+            CMS.SaveCanvas(canv, "{}/{}/{}/{}.pdf".format(output_path, subfolder_name, trigger, plot_name))
 
 if __name__ == "__main__":
     args = parse_arguments()
     
-    trigger_list: List[str] = []
     files: List[str] = []
-    
-    if args.triggerpath:
-        trigger_list = file_read_lines(args.triggerpath)
-    elif args.triggerlist:
-        trigger_list = [s.strip() for s in args.triggerlist.split(',')]
 
     if args.filepath:
         files = file_read_lines(args.filepath)
@@ -157,13 +228,14 @@ if __name__ == "__main__":
     config_file = args.config
     config = read_config_file(config_file)
 
-
-
+    plots = config.sections()
     print("Producing plots...")
     start = time.time()
     root_files = [ROOT.TFile.Open(path) for path in files]
     for root_file in root_files:
-        produce_plots(root_file, output_path, config, trigger_list=trigger_list)
+        produce_plots_from_config(root_file, output_path, config, plots)
+        if args.all:
+            produce_plots_all(root_file, output_path)
 
     for root_file in root_files:
         root_file.Close()
