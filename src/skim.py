fce79ab40db7ff21e25eeb44ab3ce68736034fde
Dan Näsman <Mdan.nasman@gmail.com>
2024-11-13 10:47:37 +0100

    drop columns that containt string 'test'

--- src/skim.py
+++ src/skim.py
@@ -405,7 +405,7 @@ def run(args):
     #all_columns.extend(events_rdf.GetDefinedColumnNames())
     all_columns = [str(col) for col in all_columns \
                     if not str(col).startswith("Jet_") and not str(col).endswith("_temp") \
-                    and not str(col).startswith("L1_")]
+                    and not str(col).startswith("L1_") and not "test" in str(col).lower()]
 
     # Write and hadd the output
     if not os.path.exists(args.out):
