56a70ffb99b5113ad55ef427844cc3935d7bcc35
Nico Toikka <nico.toikka@helsinki.fi>
2025-01-17 11:11:48 +0100

    feat: initialize triggers not in file

--- src/skim.py
+++ src/skim.py
@@ -471,6 +471,12 @@ def run(args):
 
 
     # Filter based on triggers and one jet
+    # Check that the triggers are in the file
+    cols = events_rdf.GetColumnNames()
+    for trigger in triggers:
+        if trigger not in cols:
+            print(f"Trigger {trigger} not in the file") 
+            events_rdf = events_rdf.Define(trigger, "0")
     if len(triggers) == 0:
         trg_filter = "1"
     else:
