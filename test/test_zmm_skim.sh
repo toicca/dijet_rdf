aad5f65b7c158eaa36b56060c5a6103b25f6f08b
Nico Toikka <nico.toikka@helsinki.fi>
2025-02-18 15:22:28 +0100

    fix: zmm label, add jetId fix

--- test/test_zmm_skim.sh
+++ test/test_zmm_skim.sh
@@ -6,14 +6,12 @@ python3 src/main.py skim \
   --golden_json /eos/user/c/cmsdqm/www/CAF/certification/Collisions24/2024H_Golden.json \
   --triggerpath data/triggerlists/ZJET_triggers_skim.txt \
   --out out_skim \
-  --channel zjet \
+  --channel zmm \
   --nThreads 8 \
   --nsteps 20 \
   --step 1 \
-  --correction_json data/corrections/summer24_corrections.json \
-  --correction_key Run2024H \
   --progress_bar
+  # --correction_json data/corrections/summer24_corrections.json \
+  # --correction_key Run2024H \
 
 RET=$?
-
-exit $RET
